d08a012451a34a895be36c78063ec9384ab6c949
Thomas Roehr <thomas.roehr@dfki.de>
2015-07-30 18:31:52 +0200

    Update main documentation

--- src/model/OWLOntology.hpp
+++ src/model/OWLOntology.hpp
@@ -15,6 +15,22 @@
 /**
  * \mainpage OWL API in C++
  * \mainsection Managing Web Ontology Language (OWL) based information in C++
+ *
+ * In general we recommend the usage of the OWL editor protége: http://protege.stanford.edu
+ * in case you want to edit/create OWL model files.
+ * Make sure you export as RDF/XML -- this is currently the only supported
+ * format for import.
+ *
+ * Currently this library supports a limit modelling capability, i.e., the main
+ * limitations arises from the fact that modelling from user level is done
+ * mainly through NamedInvididuals.
+ * Thus, anonymous classes and complex constructs such as Collections, OneOf,
+ * UnionOf classes is currently now supported.
+ *
+ * \section Examples
+ * The following examples shall help you to understand the user interface and
+ * applicability.
+ *
  * \verbatim
 #include <owlapi/model/OWLOntology.hpp>
 
@@ -46,6 +62,79 @@ OWLOntologyAsk ask(ontology);
 IRI robot("http:://www.rock-robotics.org/2014/01/om-schema#Sherpa")
 std::vector<OWLCardinalityRestriction::Ptr> cardinalityRestrictions = ask.getCardinalityRestrictions(robot);
  \endverbatim
+
+ * \subsection Vocabularies
+ *
+ * To facilitate the handling of OWL statements, you can use inbuilt
+ * vocabularies or define your own -- see owlapi/vocabularies/OWL.hpp
+ * for a starting example including some macros that facilitate the definition
+ * of new vocabularies.
+ *
+ * Define a custom vocabulary
+ * \verbatim
+ #include <owlapi/Vocabulary.hpp>
+
+ owlapi::vocabulary::Custom custom("http://base-url/");
+ owlapi::model::IRI iri = custom.resolve("my-instance");
+ \endverbatim
+ *
+ * Use an existing vocabulary. Note that the iri for types which collide
+ * with C++ inbuilt types/keyword have to rely on the 'resolve' function, e.g.,
+ * double, float, int
+ *
+ * \verbatim
+ #include <owlapi/Vocabulary.hpp>
+
+ owlapi::model::IRI iriThing = owlapi::vocabulary::OWL::Thing();
+ owlapi::model::IRI iriDouble = owlapi::vocabulary::OWL::resolve("double");
+ \endverbatim
+ *
+ *
+ * \subsection DataProperties Handling of DataProperties
+ *
+ * Retrieving values of data properties, e.g.,
+ * when the datatype for the property is known as 'double'
+ *
+ * \verbatim
+
+ IRI instance = owlapi::vocabulary::OM::resolve("MyRobot");
+ IRI property = owlapi::vocabulary::OM::resolve("mass");
+
+ OWLLiteral::Ptr value = ask.getDataValue(instance, property);
+ double robotMass = value->getDouble();
+
+ \endverbatim
+ *
+ * Setting values of data properties, e.g.,
+ * when the datatype for the property is known as 'double'
+ * \verbatim
+
+ IRI instance = owlapi::vocabulary::OM::resolve("MyRobot");
+ IRI property = owlapi::vocabulary::OM::resolve("mass");
+
+ OWLLiteral::Ptr value = OWLLiteral::create("0.5", owlapi::vocabulary::XSD::resolve("double"));
+ tell.valueOf(instance, property, literal);
+
+ \endverbatim
+ *
+ * \section Architecture
+ *
+ * This library is a C++-Clone of the JAVA-based owlapi: http://owlapi.sourceforge.net.
+ * The motivation for implementation is to allow a consistent application of
+ * C/C++-based programs on robotic systems -- especially since we are intending
+ * to target small/embedded devices.
+ *
+ * Furthermore, the embedded reasoner FACT++ (Reasoner for the SROIQ(D) Description Logic v1.6.2) is actually written in C++
+ * and thus can be access almost directly.
+ * This implementation is not as complete as the orignal JAVA-based
+ * one, but provides core features to handle ontologies and supports also some more complex
+ * modelling using qualified cardinality restrictions.
+ *
+ * The user can load the model from file, and manipulate the resulting ontology
+ * using two separate accessor classes: the Tell and the Ask interface --
+ * inspiration has been taken from the DIG Interface
+ * (http://dl.kr.org/dig/interface.html) to use this split.
+ *
  */
 namespace owlapi {
 
