70a30103724f5a997dbba4d3126e9246b8b91b3c
Thomas Roehr <thomas.roehr@dfki.de>
2015-12-29 11:19:29 +0100

    cmake: add cmake integration to facilitate installation of ontologies to standard folder

---
+++ modules/owlapi.cmake
@@ -0,0 +1,17 @@
+find_program(OWLAPI owlapi-utils)
+if(NOT OWLAPI)
+    message(FATAL "Could not find program: 'owlapi-utils' -- did you install 'owlapi' (and updated you environmental variables)")
+endif(NOT OWLAPI)
+
+# Install an ontology into the share folder
+# by renaming the file to a canonized filename
+# see 'owlapi-utils --help' for information
+function(owlapi_install_ontology URL OWL_FILE)
+exec_program(${OWLAPI}
+    ARGS "-c ${URL}"
+    OUTPUT_VARIABLE URL_OWL)
+
+INSTALL(FILES ${OWL_FILE}
+    DESTINATION share/ontologies
+    RENAME ${URL_OWL}.owl)
+endfunction(owlapi_install_ontology)
