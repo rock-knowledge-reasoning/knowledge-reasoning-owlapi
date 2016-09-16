7ff2ce65938ab524e25355b0d5978addf75e3e8c
Thomas Roehr <thomas.roehr@dfki.de>
2016-09-16 12:53:43 +0200

    utils: update ontology reader

--- src/utils/OntologyReaderMain.cpp
+++ src/utils/OntologyReaderMain.cpp
@@ -1,24 +1,33 @@
 #include <iostream>
-#include <owlapi/io/OWLOntologyReader.hpp>
 #include <owlapi/io/OWLOntologyIO.hpp>
+#include <owlapi/model/OWLOntologyAsk.hpp>
 
 using namespace owlapi::model;
 
+void printUsage(int argc, char** argv)
+{
+        std::cout << "usage: " << argv[0] << " <ontology-filename>" << std::endl;
+}
+
 int main(int argc, char** argv)
 {
-    if(argc != 2)
+    if(argc < 2)
     {
-        std::cout << "usage: " << argv[0] << " <ontology-filename>" << std::endl;
+        printUsage(argc, argv);
         return 0;
     }
 
     std::string filename(argv[1]);
-    owlapi::io::OWLOntologyReader reader;
-    OWLOntology::Ptr ontology = reader.fromFile(filename);
+    if(filename == "-h" || filename == "--help")
+    {
+        printUsage(argc, argv);
+        return 0;
+    }
 
-    std::string outFilename("/tmp/ontology-out.owl");
-    owlapi::io::OWLOntologyIO::write(outFilename, ontology);
+    OWLOntology::Ptr ontology = owlapi::io::OWLOntologyIO::fromFile(filename);
 
-    std::cout << "Written output to '" << outFilename << "'" << std::endl;
+    OWLOntologyAsk ask(ontology);
+    std::cout << "All classes:  " << ask.allClasses();
+
     return 0;
 }
