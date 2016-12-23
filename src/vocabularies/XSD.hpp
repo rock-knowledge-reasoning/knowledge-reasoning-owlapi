d34043b0998a60c0a8cba118b899d2785e08ef7a
Thomas Roehr <thomas.roehr@dfki.de>
2016-12-23 16:15:45 +0100

    vocabulary: update XSD vocabulary

--- src/vocabularies/XSD.hpp
+++ src/vocabularies/XSD.hpp
@@ -11,7 +11,9 @@ namespace vocabulary {
 /**
  * For inbuilt type in c++: long, int, short, double, float
  * use the syntax XSD::resolve("long")
+ * e.g., http://www.w3.org/2001/XMLSchema#int
  * \see http://www.w3.org/TR/owl2-syntax/#Real_Numbers.2C_Decimal_Numbers.2C_and_Integers
+ * \see https://www.w3.org/TR/xmlschema11-2/
  */
 class XSD
 {
@@ -43,8 +45,8 @@ public:
     //VOCABULARY_ADD_WORD(float);
 
     VOCABULARY_ADD_WORD(string);
-    VOCABULARY_ADD_WORD(normalizedString);
     VOCABULARY_ADD_WORD(token);
+    VOCABULARY_ADD_WORD(normalizedString);
     VOCABULARY_ADD_WORD(language);
     VOCABULARY_ADD_WORD(Name);
     VOCABULARY_ADD_WORD(QName);
@@ -56,15 +58,21 @@ public:
     VOCABULARY_ADD_WORD(base64Binary);
 
     VOCABULARY_ADD_WORD(dateTime);
+    VOCABULARY_ADD_WORD(dateTimeStamp);
     VOCABULARY_ADD_WORD(duration);
+    VOCABULARY_ADD_WORD(dayTimeDuration);
+    VOCABULARY_ADD_WORD(yearMonthDuration);
     VOCABULARY_ADD_WORD(date);
     VOCABULARY_ADD_WORD(time);
-    VOCABULARY_ADD_WORD(gYearMonth);
-    VOCABULARY_ADD_WORD(gYear);
-    VOCABULARY_ADD_WORD(gMonthDay);
     VOCABULARY_ADD_WORD(gDay);
     VOCABULARY_ADD_WORD(gMonth);
+    VOCABULARY_ADD_WORD(gMonthDay);
+    VOCABULARY_ADD_WORD(gYear);
+    VOCABULARY_ADD_WORD(gYearMonth);
 
+    VOCABULARY_ADD_WORD(anyType);
+    VOCABULARY_ADD_WORD(anySimpleType);
+    VOCABULARY_ADD_WORD(anyAtomicType);
     VOCABULARY_ADD_WORD(anyURI);
     VOCABULARY_ADD_WORD(ID);
     VOCABULARY_ADD_WORD(IDREF);
@@ -73,7 +81,20 @@ public:
     VOCABULARY_ADD_WORD(ENTITIES);
     VOCABULARY_ADD_WORD(NOTATION);
 
-    VOCABULARY_ADD_WORD(dateTimeStamp);
+    // Facets
+    VOCABULARY_ADD_WORD(whiteSpace);
+
+    VOCABULARY_ADD_WORD(totalDigits);
+    VOCABULARY_ADD_WORD(fractionDigits);
+    VOCABULARY_ADD_WORD(pattern);
+    VOCABULARY_ADD_WORD(enumeration);
+    VOCABULARY_ADD_WORD(minInclusive);
+    VOCABULARY_ADD_WORD(maxInclusive);
+    VOCABULARY_ADD_WORD(minExclusive);
+    VOCABULARY_ADD_WORD(maxExclusive);
+    VOCABULARY_ADD_WORD(assertions);
+
+    VOCABULARY_ADD_WORD(restriction);
 };
 
 } // end namespace vocabulary
