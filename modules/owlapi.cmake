de039917bed33ab89cb3530a63b55bf7cc21558e
Thomas Roehr <thomas.roehr@dfki.de>
2019-02-15 17:27:48 +0100

    owlapi.cmake: avoid using longest suffix and allow validation of ontologies before installation file

--- modules/owlapi.cmake
+++ modules/owlapi.cmake
@@ -10,11 +10,30 @@ endif()
 # by renaming the file to a canonized filename
 # see 'owlapi-utils --help' for information
 function(owlapi_install_ontology URL OWL_FILE)
-exec_program(${OWLAPI}
-    ARGS "-c ${URL}"
-    OUTPUT_VARIABLE URL_OWL)
+execute_process(COMMAND ${OWLAPI} -c ${URL}
+    OUTPUT_VARIABLE URL_OWL
+    ERROR_QUIET
+    OUTPUT_STRIP_TRAILING_WHITESPACE)
 
-get_filename_component(OWL_SUFFIX ${OWL_FILE} EXT)
+string(REGEX MATCH "\\.[a-z]+$" OWL_SUFFIX ${OWL_FILE})
+
+# Validate the ontology
+execute_process(
+    COMMAND rapper -q --guess ${CMAKE_SOURCE_DIR}/${OWL_FILE} -o rdfxml
+    OUTPUT_FILE ${CMAKE_BINARY_DIR}/${URL_OWL}.rdf
+    ERROR_QUIET
+    RESULT_VARIABLE RAPPER_EXIT_STATUS
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    ERROR_STRIP_TRAILING_WHITESPACE
+)
+if(RAPPER_EXIT_STATUS EQUAL 0)
+    message(STATUS "Conversion of ontology file ${OWL_FILE} to rdfxml "
+        "succeeded -- your ontology appears to be well formatted")
+else()
+    message(SEND_ERROR "Conversion of ontology file ${OWL_FILE} to rdfxml "
+        "failed -- your ontology seems to be malformatted/invalid")
+endif()
+
 INSTALL(FILES ${OWL_FILE}
     DESTINATION share/ontologies
     RENAME ${URL_OWL}${OWL_SUFFIX})
